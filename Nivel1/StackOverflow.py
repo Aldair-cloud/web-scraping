7afd447b928faad94aec14ace61ffc4985132fc7
Aldair Sánchez Pérez <audairu@gmail.com>
2024-11-13 11:54:59 -0500

    stackoverflow

--- Nivel1/StackOverflow.py
+++ Nivel1/StackOverflow.py
@@ -14,9 +14,27 @@ soup = BeautifulSoup(respuesta.text, "html.parser")
 contenedor_de_preguntas = soup.find(id="questions")
 lista_de_preguntas = contenedor_de_preguntas.find_all('div', class_="s-post-summary")
 
+# for pregunta in lista_de_preguntas:
+#   texto_pregunta = pregunta.find('h3').text
+#   descripcion_pregunta = pregunta.find(class_='s-post-summary--content-excerpt').text
+#   #replace es una funcion para reemplazar, en este caso lo usariamos para los espacios y los saltos de linea
+#   #strip elimina los espacios que estan antes o despues de una cadena
+#   descripcion_pregunta = descripcion_pregunta.replace('\n', '').replace('\r','').strip()
+#   print(texto_pregunta)
+#   print(descripcion_pregunta)
+
+#   print()
+
+#Con beautifulsoup
 for pregunta in lista_de_preguntas:
-  texto_pregunta = pregunta.find('h3').text
-  descripcion_pregunta = pregunta.find(class_='s-post-summary--content-excerpt').text
+  elemento_texto_pregunta = pregunta.find('h3')
+  texto_pregunta = elemento_texto_pregunta.text
+
+  descripcion_pregunta = elemento_texto_pregunta.find_next_sibling('div').text
+  
+  #replace es una funcion para reemplazar, en este caso lo usariamos para los espacios y los saltos de linea
+  #strip elimina los espacios que estan antes o despues de una cadena
+  descripcion_pregunta = descripcion_pregunta.replace('\n', '').replace('\r','').strip()
   print(texto_pregunta)
   print(descripcion_pregunta)
 
